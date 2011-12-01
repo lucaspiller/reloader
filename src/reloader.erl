c6f7f9dc54d409bff672a6b89b58d41a6d581b77
Luca Spiller <luca@stackednotion.com>
2011-12-01 11:55:15 +0000

    Change return of #reload to support multiple return values

--- src/reloader.erl
+++ src/reloader.erl
@@ -130,20 +130,21 @@ reload(Module) ->
     case code:load_file(Module) of
         {module, Module} ->
             io:format(" ok.~n"),
-            case erlang:function_exported(Module, test, 0) of
+            TestResult = case erlang:function_exported(Module, test, 0) of
                 true ->
-                    io:format(" - Calling ~p:test() ...", [Module]),
+                    io:format(" - Running tests for ~p ...", [Module]),
                     case catch Module:test() of
                         ok ->
                             io:format(" ok.~n"),
-                            reload;
-                        Reason ->
-                            io:format(" fail: ~p.~n", [Reason]),
-                            reload_but_test_failed
+                            test_passed;
+                        TestFailureReason ->
+                            io:format(" fail: ~p.~n", [TestFailureReason]),
+                            test_failed
                     end;
                 false ->
-                    reload
-            end;
+                    no_test
+            end,
+            {reload, TestResult};
         {error, Reason} ->
             io:format(" fail: ~p.~n", [Reason]),
             error
